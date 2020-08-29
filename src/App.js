0aaf241efdc686058f60d318186fadebac45c1fa
LoneStar <ernieurzua@gmail.com>
2020-08-29 00:32:45 -0700

    added /react-portfolio to all img in db.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Main from "./pages/Main";
@@ -15,13 +15,13 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Main} /><Redirect to="/projects" />
           <Route exact path="/projects" component={Main} />
           <Route exact path="/about" component={About} />
-          <Route path='/resume' component={() => window.location = 'https://docs.google.com/document/d/1sw88-b4F8rUdlXCVsSD_dUfwirS9NjuOvVSu7f1WUTY/edit?usp=sharing'}/>
+          <Route path='/resume' component={() => window.location = 'https://docs.google.com/document/d/1sw88-b4F8rUdlXCVsSD_dUfwirS9NjuOvVSu7f1WUTY/edit?usp=sharing'} />
           {/* <Route path='/resume' component={() => <a href= 'https://docs.google.com/document/d/1sw88-b4F8rUdlXCVsSD_dUfwirS9NjuOvVSu7f1WUTY/edit?usp=sharing' target="_blank"></a>}/> */}
           {/* <Route a href="https://docs.google.com/document/d/1sw88-b4F8rUdlXCVsSD_dUfwirS9NjuOvVSu7f1WUTY/edit?usp=sharing" target="_blank"/><a/> */}
-       </Wrapper>
+        </Wrapper>
         <Footer />
       </div>
     </Router>
