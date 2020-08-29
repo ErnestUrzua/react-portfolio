6b79c3871bd6ffc6705dac580c71b441a09d212e
LoneStar <ernieurzua@gmail.com>
2020-08-28 23:54:13 -0700

    Updates

---
+++ precache-manifest.eb7850dff0057eeabdf4e314029c9970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2b7822ed724594f2b956404c42661d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7f3c38303771678755",
+    "url": "/static/css/main.6c82f07b.chunk.css"
+  },
+  {
+    "revision": "6ee517f25bc23cbcd39c",
+    "url": "/static/js/2.62518424.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.62518424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7f3c38303771678755",
+    "url": "/static/js/main.046f3e10.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
